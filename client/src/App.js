59ab854f2b721af82e758694dcdf722cd427e3f6
Grant Chen <grantchen2021@gmail.com>
2022-09-17 11:46:43 -0400

    set up backend ?

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const App = () => {
+  useEffect(() => {
+    const test = async () => {
+      console.log('bet')
+      const response = await fetch('/');
+      console.log(response)
+      const data = await response.json()
+      console.log(data)
+    }
+    test()
+
+  }, [])
   return (
     <div>App</div>
   )
