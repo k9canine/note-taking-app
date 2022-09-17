4a096c2376579e6de47cffec98b92e23c96c9725
icecapped <1027Michael@gmail.com>
2022-09-17 12:08:12 -0400

    Add start of transcription receive function

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+const receiveMessage = (message) => {
+  let msg = '';
+  const res = Json.parse(message.data);
+  const texts = {};
+  texts[res.audio_start] = res.text;
+  const keys = Object.keys(texts).sort((a, b) => a - b);
+  keys.forEach((key) => {
+    if(texts[key]) msg += ` ${texts[key]}`;
+  });
+
+}
+
 const App = () => {
   return (
     <div>App</div>
